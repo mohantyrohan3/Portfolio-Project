779d54c4a0d68b7df3dd9c140afb8c554eddd8ed
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-02-07 21:19:08 +0530

    on screen load removed

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 ScrollReveal().reveal('.card',{delay:100,duration:1000,reset:true});
-document.onload = function(){
-    window.scrollTo(0,0);
-}
-
-
 
 let back_top = document.getElementById('back_top');
 
-top.addEventListener('click',function(){
+back_top.addEventListener('click',function(){
     window.scrollTo(0,0);
 })
